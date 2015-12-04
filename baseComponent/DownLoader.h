193d63c2308897caefb55a5684c72d914cfa6ec5
guima <1770421128@qq.com>
2015-12-04 15:53:53 +0800

    第二次

---
+++ baseComponent/DownLoader.h
@@ -0,0 +1,13 @@
+//
+//  DownLoader.h
+//  baseComponent
+//
+//  Created by 鬼马 on 15/12/4.
+//  Copyright © 2015年 鬼马. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownLoader : NSObject
+
+@end
---
+++ baseComponent/DownLoader.m
@@ -0,0 +1,21 @@
+//
+//  DownLoader.m
+//  baseComponent
+//
+//  Created by 鬼马 on 15/12/4.
+//  Copyright © 2015年 鬼马. All rights reserved.
+//
+
+#import "DownLoader.h"
+
+@implementation DownLoader
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"测试一下");
+    }
+    return self;
+}
+@end
